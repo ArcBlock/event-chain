bb947dae88c748253a75a23ef95d23152d1e8360
Riley Shu <riley@arcblock.io>
2019-05-20 16:57:46 -0700

    use datepicker to enter date

--- event_chain/app/forms/event.py
+++ event_chain/app/forms/event.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
-from wtforms import StringField
+from wtforms import StringField, DateField
 from wtforms import SubmitField
 from wtforms import validators
 from wtforms.validators import DataRequired
@@ -13,9 +13,9 @@ class EventForm(FlaskForm):
                                               validators.number_range(min=1,
                                                                       max=30)
                                               ])
-    start_time = StringField("StartTime", validators=[DataRequired()])
+    start_time = DateField("StartTime", validators=[DataRequired()])
     description = StringField("Description")
-    end_time = StringField("EndTime", validators=[DataRequired()])
+    end_time = DateField("EndTime", validators=[DataRequired()])
     ticket_price = IntegerField("TicketPrice")
     address = StringField('Address')
     location = StringField('Location', validators=[DataRequired()])
